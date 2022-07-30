ecda3cf9932ce12a25f70501fb59721847dd0cd8
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-07-30 10:04:55 +0600

    project complete

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+if(document.querySelector('.TestimonialSwiper')){
+  var swiper = new Swiper(".TestimonialSwiper", {
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+      centeredSlides: true,
+      },
+      // when window width is >= 992px
+      576: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+      },
+      // when window width is >= 1400px
+      1400: {
+      slidesPerView: 3,
+      spaceBetween: 55,
+      centeredSlides: true,
+      }
+    }
+  });
+}
+
+ // tooltip active global
+ let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+ let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+   return new bootstrap.Tooltip(tooltipTriggerEl)
+ })
+
